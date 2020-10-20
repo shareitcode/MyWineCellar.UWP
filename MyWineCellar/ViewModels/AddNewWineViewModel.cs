782f5a15d781988b49f70ae9bf85c0d17cba69d6
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-10-20 23:41:31 +0200

    [1.0] WIP CRUD for wine

--- MyWineCellar/ViewModels/AddNewWineViewModel.cs
+++ MyWineCellar/ViewModels/AddNewWineViewModel.cs
@@ -1,44 +1,37 @@
-﻿using MyWineCellar.Helpers;
-using MyWineCellar.Models;
+﻿using MyWineCellar.DTO;
+using MyWineCellar.Helpers;
 using MyWineCellar.Services;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace MyWineCellar.ViewModels
 {
 	internal sealed class AddNewWineViewModel : BaseViewModel
 	{
-		public Wine Wine { get; set; }
+		public WineDto Wine { get; set; } = new WineDto();
 
 		private string _errorMessage = string.Empty;
 		public string ErrorMessage
 		{
 			get => this._errorMessage;
-			set
-			{
-				this._errorMessage = value;
-				this.OnPropertyChanged();
-			}
+			set => this.Set(ref this._errorMessage, value);
 		}
 
 		private bool _errorMessageIsVisible;
 		public bool ErrorMessageIsVisible
 		{
 			get => this._errorMessageIsVisible;
-			set
-			{
-				this._errorMessageIsVisible = value;
-				this.OnPropertyChanged();
-			}
+			set => this.Set(ref this._errorMessageIsVisible, value);
 		}
 
 		public ICommand AddNewWineCommand => new RelayCommand(this.AddNewWine);
 
-		public AddNewWineViewModel() => this.Wine = new Wine();
-
 		public void AddNewWine()
 		{
-			if (Session.Instance.Add(nameof(this.Wine), this.Wine))
+			if (Session.Instance.IsExist("Wines"))
 			{
+				Session.Instance.Get<ObservableCollection<WineDto>>("Wines").Add(this.Wine);
+				this.Wine = null;
 				NavigationService.GoBack();
 			}
 			else
