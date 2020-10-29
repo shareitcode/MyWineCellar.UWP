45ab120a43303598a8a0c23516b9f702450710a2
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-10-29 19:57:09 +0100

    [1.0] [#2] WIP manage form control : manage error message

---
+++ MyWineCellar/Converters/BooleanToVisibleConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace MyWineCellar.Converters
+{
+	internal sealed class BooleanToVisibleConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language) => value is bool valueAsBool
+																							 && valueAsBool ? Visibility.Visible : Visibility.Collapsed;
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language) => (value is Visibility visibility && visibility == Visibility.Visible);
+	}
+}
--- MyWineCellar/Helpers/UserInterfaceHelper.cs
+++ MyWineCellar/Helpers/UserInterfaceHelper.cs
@@ -1,20 +1,26 @@
-﻿using System;
+﻿using System.Globalization;
+using Windows.UI;
 using Windows.UI.Xaml.Media;
 
 namespace MyWineCellar.Helpers
 {
-    internal static class UserInterfaceHelper
-    {
-        public static SolidColorBrush GetSolidColorBrushFromHexadecimal(string hexadecimal)
-        {
-            hexadecimal = hexadecimal.Replace("#", string.Empty);
+	internal sealed class UserInterfaceHelper
+	{
+		/// <summary>
+		/// Parse hexadecimal string to byte for get color brush from ARGB bytes
+		/// </summary>
+		/// <param name="hexadecimal">Format example: #FFFFFFFF</param>
+		/// <returns></returns>
+		public static SolidColorBrush GetSolidColorBrushFromHexadecimal(string hexadecimal)
+		{
+			string hexadecimalWithoutHash = hexadecimal.Replace("#", string.Empty);
 
-            byte a = (byte)Convert.ToUInt32(hexadecimal.Substring(0, 2), 16);
-            byte r = (byte)Convert.ToUInt32(hexadecimal.Substring(2, 2), 16);
-            byte g = (byte)Convert.ToUInt32(hexadecimal.Substring(4, 2), 16);
-            byte b = (byte)Convert.ToUInt32(hexadecimal.Substring(6, 2), 16);
+			byte.TryParse(hexadecimalWithoutHash.Substring(0, 2), NumberStyles.HexNumber, new NumberFormatInfo(), out byte a);
+			byte.TryParse(hexadecimalWithoutHash.Substring(2, 2), NumberStyles.HexNumber, new NumberFormatInfo(), out byte r);
+			byte.TryParse(hexadecimalWithoutHash.Substring(4, 2), NumberStyles.HexNumber, new NumberFormatInfo(), out byte g);
+			byte.TryParse(hexadecimalWithoutHash.Substring(6, 2), NumberStyles.HexNumber, new NumberFormatInfo(), out byte b);
 
-            return new SolidColorBrush(Windows.UI.Color.FromArgb(a, r, g, b));
-        }
-    }
+			return new SolidColorBrush(Color.FromArgb(a, r, g, b));
+		}
+	}
 }
