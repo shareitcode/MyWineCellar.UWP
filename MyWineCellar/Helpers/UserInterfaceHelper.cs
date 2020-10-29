c465f84b90f6dd7ab3eb2ab8ce2264fa11757860
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-10-29 17:27:29 +0100

    [1.0] [#2] WIP manage form control : add error color

---
+++ MyWineCellar/Helpers/UserInterfaceHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml.Media;
+
+namespace MyWineCellar.Helpers
+{
+    internal static class UserInterfaceHelper
+    {
+        public static SolidColorBrush GetSolidColorBrushFromHexadecimal(string hexadecimal)
+        {
+            hexadecimal = hexadecimal.Replace("#", string.Empty);
+
+            byte a = (byte)Convert.ToUInt32(hexadecimal.Substring(0, 2), 16);
+            byte r = (byte)Convert.ToUInt32(hexadecimal.Substring(2, 2), 16);
+            byte g = (byte)Convert.ToUInt32(hexadecimal.Substring(4, 2), 16);
+            byte b = (byte)Convert.ToUInt32(hexadecimal.Substring(6, 2), 16);
+
+            return new SolidColorBrush(Windows.UI.Color.FromArgb(a, r, g, b));
+        }
+    }
+}
--- MyWineCellar/Views/AddNewWinePage.xaml.cs
+++ MyWineCellar/Views/AddNewWinePage.xaml.cs
@@ -1,16 +1,61 @@
-﻿using MyWineCellar.ViewModels;
+﻿using Microsoft.Xaml.Interactions.Core;
+using Microsoft.Xaml.Interactivity;
+using MyWineCellar.ViewModels;
 using Windows.UI.Xaml.Controls;
 
 namespace MyWineCellar.Views
 {
-	internal sealed partial class AddNewWinePage : Page
-	{
-		private AddNewWineViewModel AddNewWineViewModel { get; } = new AddNewWineViewModel();
+    internal sealed partial class AddNewWinePage : Page
+    {
+        private AddNewWineViewModel AddNewWineViewModel { get; } = new AddNewWineViewModel();
 
-		public AddNewWinePage()
-		{
-			this.InitializeComponent();
-			this.DataContext = this.AddNewWineViewModel;
-		}
-	}
+        public AddNewWinePage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this.AddNewWineViewModel;
+            SetBehaviorsForUsersControls();
+        }
+
+        private void SetBehaviorsForUsersControls()
+        {
+            this.SetBehaviorsForProducerTextBox();
+            this.SetBehaviorsForCountryTextBox();
+        }
+
+        private void SetBehaviorsForProducerTextBox()
+        {
+            Interaction.SetBehaviors(this.TextBoxProducer, new BehaviorCollection
+            {
+                new EventTriggerBehavior
+                {
+                    EventName = "LostFocus",
+                    Actions =
+                        {
+                            new InvokeCommandAction
+                            {
+                                Command = this.AddNewWineViewModel.CheckProducerValidityCommand
+                            }
+                        }
+                }
+            });
+        }
+
+        private void SetBehaviorsForCountryTextBox()
+        {
+            Interaction.SetBehaviors(this.TextBoxCountry, new BehaviorCollection
+            {
+                new EventTriggerBehavior
+                {
+                    EventName = "LostFocus",
+                    Actions =
+                        {
+                            new InvokeCommandAction
+                            {
+                                Command = this.AddNewWineViewModel.CheckProducerValidityCommand
+                            }
+                        }
+                }
+            });
+        }
+    }
 }
