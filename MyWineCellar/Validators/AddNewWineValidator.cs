0be637d42406d2fe8ecc0ecfd495a864fea843da
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-12-02 16:52:06 +0100

    [1.0] [#2] WIP Update validation form control

---
+++ MyWineCellar/Validators/AddNewWineValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MyWineCellar.Models;
+
+namespace MyWineCellar.Validators
+{
+	internal sealed class AddNewWineValidator : AbstractValidator<AddWineModel>
+	{
+		public AddNewWineValidator()
+		{
+			this.RuleFor(wine => wine.Producer).NotNull().NotEmpty().WithMessage("Please specify a wine producer");
+			this.RuleFor(wine => wine.Country).NotNull().NotEmpty().WithMessage("Please specify a wine country");
+			this.RuleFor(wine => wine.Region).NotNull().NotEmpty().WithMessage("Please specify a wine region");
+			this.RuleFor(wine => wine.Appellation).NotNull().NotEmpty().WithMessage("Please specify a wine appellation");
+			this.RuleFor(wine => wine.Parcel).NotNull().NotEmpty().WithMessage("Please specify a wine parcel");
+			this.RuleFor(wine => wine.Vintage).NotNull().DependentRules(() =>
+			{
+				this.RuleFor(wine => wine.Vintage).GreaterThan(vintage => 0);
+			}).WithMessage("Please specify a wine vintage");
+			this.RuleFor(wine => wine.Quantity).NotNull().DependentRules(() =>
+			{
+				this.RuleFor(wine => wine.Quantity).GreaterThan(vintage => 0);
+			}).WithMessage("Please specify a wine quantity");
+			this.RuleFor(wine => wine.Color).GreaterThan(vintage => 0).WithMessage("Please specify a wine color");
+			this.RuleFor(wine => wine.AcquisitionMeans).GreaterThan(vintage => 0).WithMessage("Please specify a wine acquisition means");
+		}
+
+		internal static AddNewWineValidator GetValidator() => new AddNewWineValidator();
+	}
+}
--- MyWineCellar/ViewModels/AddNewWineViewModel.cs
+++ MyWineCellar/ViewModels/AddNewWineViewModel.cs
@@ -1,15 +1,14 @@
 ﻿using MyWineCellar.Helpers;
 using MyWineCellar.Models;
 using MyWineCellar.Services;
+using MyWineCellar.Validators;
 using System;
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using FluentValidation.Results;
 
 namespace MyWineCellar.ViewModels
 {
@@ -17,138 +16,42 @@ namespace MyWineCellar.ViewModels
 	{
 		public AddWineModel Wine { get; set; } = new AddWineModel();
 
-		public IEnumerable<String> WineColors { get; } = Constants.WineColors;
+		public IEnumerable<string> WineColors { get; } = Constants.WineColors;
 
 		public IEnumerable<string> AcquisitionMeans { get; } = Constants.AcquisitionMeans;
 
-		private bool _addNewWineButtonIsEnabled;
-		public bool AddNewWineButtonIsEnabled
+		private string _errorMessage;
+		public string ErrorMessage
 		{
-			get => this._addNewWineButtonIsEnabled;
-			set => this.Set(ref this._addNewWineButtonIsEnabled, value);
-		}
-
-		private bool _producerIsValid;
-		public bool ProducerIsValid
-		{
-			get => this._producerIsValid;
-			set
-			{
-				this.Set(ref this._producerIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _countryIsValid;
-		public bool CountryIsValid
-		{
-			get => this._countryIsValid;
-			set
-			{
-				this.Set(ref this._countryIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _regionIsValid;
-		public bool RegionIsValid
-		{
-			get => this._regionIsValid;
-			set
-			{
-				this.Set(ref this._regionIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _appellationIsValid;
-		public bool AppellationIsValid
-		{
-			get => this._appellationIsValid;
-			set
-			{
-				this.Set(ref this._appellationIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _parcelIsValid;
-		public bool ParcelIsValid
-		{
-			get => this._parcelIsValid;
-			set
-			{
-				this.Set(ref this._parcelIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _vintageIsValid;
-		public bool VintageIsValid
-		{
-			get => this._vintageIsValid;
-			set
-			{
-				this.Set(ref this._vintageIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _quantityIsValid;
-		public bool QuantityIsValid
-		{
-			get => this._quantityIsValid;
-			set
-			{
-				this.Set(ref this._quantityIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _colorIsValid;
-		public bool ColorIsValid
-		{
-			get => this._colorIsValid;
-			set
-			{
-				this.Set(ref this._colorIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
-		}
-
-		private bool _acquisitionMeansIsValid;
-        public bool AcquisitionMeansIsValid
-		{
-			get => this._acquisitionMeansIsValid;
-			set
-			{
-				this.Set(ref this._acquisitionMeansIsValid, value);
-				this.AddNewWineButtonIsEnabled = this.AllEntriesAreValid();
-			}
+			get => this._errorMessage;
+			set => this.Set(ref this._errorMessage, value);
 		}
 
 		public ICommand AddNewWineCommand => new RelayCommand(async () => await this.AddNewWine());
-
 
 		public async Task AddNewWine()
 		{
 			try
 			{
 				AddWineModel wine = this.Wine;
+				ValidationResult validationResult = await AddNewWineValidator.GetValidator().ValidateAsync(wine);
+				if (validationResult.IsValid)
+					NavigationService.GoBack();
+				else
+				{
+					StringBuilder stringBuilder = new StringBuilder();
+					foreach (ValidationFailure validationFailure in validationResult.Errors)
+					{
+						stringBuilder.AppendLine($"- {validationFailure.ErrorMessage}");
+					}
+					this.ErrorMessage = stringBuilder.ToString();
+				}
 				//await WineRepository.Add(this.Wine);
-				NavigationService.GoBack();
 			}
 			catch (Exception e)
 			{
 				Console.WriteLine(e);
 			}
 		}
-
-		private bool AllEntriesAreValid()
-		{
-			return this.ProducerIsValid && this.CountryIsValid && this.RegionIsValid
-					&& this.AppellationIsValid && this.ParcelIsValid && this.VintageIsValid
-					&& this.QuantityIsValid && this.ColorIsValid && this.AcquisitionMeansIsValid;
-		}
-    }
+	}
 }
