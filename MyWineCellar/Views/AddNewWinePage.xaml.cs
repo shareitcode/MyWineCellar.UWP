3da103b6b99f7811c2e5ea83118e75d0d15b5e36
shareitcode <45316299+shareitcode@users.noreply.github.com>
2020-12-02 16:51:28 +0100

    [1.0] [ISSUE #1 #2] Update navigation implementation

--- MyWineCellar/Views/AddNewWinePage.xaml.cs
+++ MyWineCellar/Views/AddNewWinePage.xaml.cs
@@ -1,7 +1,6 @@
-﻿using MyWineCellar.ViewModels;
-using Windows.UI.Core;
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using MyWineCellar.ViewModels;
 using Windows.UI.Xaml.Controls;
-using Microsoft.Toolkit.Uwp.UI.Controls;
 
 namespace MyWineCellar.Views
 {
@@ -13,17 +12,6 @@ namespace MyWineCellar.Views
         {
             this.InitializeComponent();
             this.DataContext = this.AddNewWineViewModel;
-            SystemNavigationManager.GetForCurrentView().BackRequested += this.AddNewWineView_BackRequested;
-            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-        }
-
-        private void AddNewWineView_BackRequested(object sender, BackRequestedEventArgs e)
-        {
-            if (this.Frame.CanGoBack)
-            {
-                this.Frame.GoBack();
-                e.Handled = true;
-            }
         }
 
         private void InAppNotification_OnClosed(object sender, InAppNotificationClosedEventArgs e)
